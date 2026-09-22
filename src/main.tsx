dd121da8cfc15f3f689ce13286372a973870bd07
bakytnur <bishkek-2020@mail.ru>
2026-09-22 17:14:35 +0600

    Updates

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles/fonts.css";
 import "./styles/index.css";
 import "./styles/btn.css";
 import { AppProviders } from "./app/providers";
@@ -9,7 +10,9 @@ import { AppProviders } from "./app/providers";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppProviders>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
         <App />
       </BrowserRouter>
     </AppProviders>
